7505b30bde101fb2a58e253775ad687f2eaf0d68
sami suliman <sami.bco@gmail.com>
2019-08-27 04:47:23 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -20,3 +20,6 @@ emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
 parser1 = EmailParser.new(emails1)
 parser1.parse 
 
+emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+parser2 = EmailParser.new(emails2)
+parser2.parse
