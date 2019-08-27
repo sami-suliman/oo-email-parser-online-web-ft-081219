03170f3248390e3cbca8ad7976f525f94fe8e36b
sami suliman <sami.bco@gmail.com>
2019-08-27 04:45:51 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,7 +6,7 @@
 class EmailParser
   attr_accessor :name, :csv_emails
   
-  def initialize
+  def initialize(csv_emails)
     @csv_emails = csv_emails
   end 
   
@@ -17,10 +17,3 @@ class EmailParser
   end 
 end 
 
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
